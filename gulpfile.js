0f9d91057212150612f963856eb39c878c56c9fb
Sintu Singh <ssi139@sapient.com>
2017-05-07 22:05:15 +0530

    'post-css'

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,16 @@ var sourcemaps = require('gulp-sourcemaps');
 
 // Plugins
 var precss = require('precss');
+// var cssnano = require('cssnano'); disable for now and see unminified css
 var lost = require('lost');
+var cssnext = require('cssnext');
 var autoprefixer = require('autoprefixer');
 var postcssImport = require("postcss-import")
 
 gulp.task('runPostCss', function () {
 	return gulp.src('app/stylesheet/precss/style.css')
 		.pipe(sourcemaps.init())
-		.pipe(postcss([precss, lost, autoprefixer, postcssImport]))
+		.pipe(postcss([precss, lost, autoprefixer, postcssImport, cssnext]))
 		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest('app/assets/css/'));
 });
