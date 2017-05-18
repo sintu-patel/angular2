438d8df815878a15eccd34b06e04e96045645ec5
Sintu Singh <ssi139@sapient.com>
2017-05-18 11:06:28 +0530

    'design'

--- app/code/modules/components/server/data-main.js
+++ app/code/modules/components/server/data-main.js
@@ -6,7 +6,7 @@ System.register([], function(exports_1, context_1) {
         setters:[],
         execute: function() {
             exports_1("DATA", DATA = {
-                "heading": "Informatin Tracking System",
+                "heading": "Informatin Tracking",
                 "data": [
                     {
                         "issue": "Is Taiwan a country?",
@@ -14,8 +14,18 @@ System.register([], function(exports_1, context_1) {
                         "readMore": "http://www.google.com"
                     },
                     {
-                        "issue": "Is Taiwan a country?",
-                        "resolution": "The question is really hard to answer because the terms Taiwan and country are somewhat ambiguous.",
+                        "issue": "What is a country?",
+                        "resolution": "A country is a region that is identified as a distinct national entity in political geography. A country may be an independent sovereign state or one that is occupied by another state, as a non-sovereign or formerly sovereign political division, or a geographic region associated with sets of previously independent or differently associated people with distinct political characteristics.",
+                        "readMore": "http://www.google.com"
+                    },
+                    {
+                        "issue": "Was Taiwan historically part of China?",
+                        "resolution": "Historically, Taiwan was not formally part of China until Ming Dynasty. There was troops sent to Taiwan like some expedition activity as early as Eastern Han Dynasty, but no goverment established so it was not part of China then. At that time Taiwan was refered to as Yizhou",
+                        "readMore": "http://www.google.com"
+                    },
+                    {
+                        "issue": "In what sense can Taiwan be recognized as a country?",
+                        "resolution": "Taiwan has its own local ethnic group, they had been settled there long before Zheng conquered Taiwan. So if we dates back to 300 years ago, Taiwan used to be independent, as an island. But that doesn’t make much sense to me. There is no exile goverment for local Taiwanese now. And if Taiwan go independent in this sense, how should we deal with Kinmen and Matsu, aka, the Fujian province in free area?",
                         "readMore": "http://www.google.com"
                     }
                 ]
--- gulpfile.js
+++ gulpfile.js
@@ -9,11 +9,12 @@ var lost = require('lost');
 var cssnext = require('cssnext');
 var autoprefixer = require('autoprefixer');
 var postcssImport = require("postcss-import")
+var vars = require('postcss-simple-vars');
 
 gulp.task('runPostCss', function () {
 	return gulp.src('app/stylesheet/precss/style.css')
 		.pipe(sourcemaps.init())
-		.pipe(postcss([precss, lost, autoprefixer, postcssImport, cssnext]))
+		.pipe(postcss([vars, precss, lost, autoprefixer, postcssImport, cssnext]))
 		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest('app/assets/css/'));
 });
