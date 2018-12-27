60025b0f7c3949de3e1118fa1ced2db3d1a4ec02
ssingh172 <ssingh172@sapient.com>
2018-12-27 15:17:41 -0500

    Remove hash

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -12,8 +12,8 @@ module.exports = webpackMerge(commonConfig, {
   output: {
     path: helpers.root('dist'),
     publicPath: '/',
-    filename: '[name].[hash].js',
-    chunkFilename: '[id].[hash].chunk.js'
+    filename: '[name].js',
+    chunkFilename: '[id].chunk.js'
   },
 
   plugins: [
