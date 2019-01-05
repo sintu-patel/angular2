665115dbf54b2cb3bd498df6a4c015cf02a7a665
ssingh172 <ssingh172@sapient.com>
2019-01-04 20:37:09 -0500

    Update webhook

--- api/routes/cmsRoute.js
+++ api/routes/cmsRoute.js
@@ -1,5 +1,7 @@
 'use strict';
 var fs = require('fs');
+var request = require('request');
+var { apiServerConfig } = require('../../app/code/config/app.main.config');
 function index(req, res) {
 	fs.readFile('index.html', 'utf8', function(err, text){
 	    res.send(text);
@@ -7,5 +9,28 @@ function index(req, res) {
 }
 module.exports = function(app) {
 	app.route('/').get(index);
+	app.route('/payload').post(sendApiPayload);
 	app.route('*').get(index);
+}
+
+function sendApiPayload(req, res) {
+	var data = req.body;
+  	var payload = data.payload;
+  	payload = JSON.parse(payload);
+
+	const options = {
+		url: `${apiServerConfig.apiHost}/payload`,
+		method: 'post',
+		json: payload
+	  }
+	  request(options, (error, response, body) => {
+			if (error) {
+				console.log('Error in getting data');
+				  console.log('Error');
+			}
+			console.log('get data success');
+			if (response) {
+				res.send('success');
+			}
+		});
 }
--- set-env-config.js
+++ set-env-config.js
@@ -10,3 +10,12 @@ export const apiServerConfig = {
 
 var apiServerConfigFile = './app/code/config/app.main.config.ts';
 fs.writeFileSync(apiServerConfigFile, fileContent, "utf8");
+
+var fileContentJS = `
+// Host Configuration
+exports.apiServerConfig = {
+    apiHost: '${apiHost}'
+}`;
+
+var apiServerConfigFileJS = './app/code/config/app.main.config.js';
+fs.writeFileSync(apiServerConfigFileJS, fileContentJS, "utf8");
